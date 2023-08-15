9684d069c9fdbaf654f9186930bb27500dc8ec57
choieastsea <choieastsea@gmail.com>
2023-08-16 06:42:46 +0900

    [FIX] env variable 오타 수정

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DB_NANE'),
+        'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
